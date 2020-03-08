d06b0557304a4a7c3dd59f4f9d0a5a110fd066a1
戴涵文 <2235356876@qq.com>
2020-03-08 15:57:55 +0800

    创建项目-lij

---
+++ crawling-server/src/main/java/com/crawling/crawlingserver/CrawlingServerApplication.java
@@ -0,0 +1,19 @@
+package com.crawling.crawlingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableSwagger2
+public class CrawlingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlingServerApplication.class, args);
+    }
+
+}
---
+++ crawling-server/src/test/java/com/crawling/crawlingserver/CrawlingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawling.crawlingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
